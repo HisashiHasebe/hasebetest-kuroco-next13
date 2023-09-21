92be219ca2b143f28ab596823e6416ba3c3eab77
HisashiHasebe <hhasebe@diverta.co.jp>
2023-09-21 17:37:43 +0800

    test

--- app/news/[slug]/page.jsx
+++ app/news/[slug]/page.jsx
@@ -1,18 +1,22 @@
 // Dynamic routes; create a page for each ticket ID
-'use client';
+export async function generateStaticParams() {
+    const contents = await fetch(process.env.NEXT_PUBLIC_BASE_URL + '/rcms-api/1/news').then((res) => res.json())
+    return contents.list.map((content) => ({
+        slug: `${content.topics_id}`,
+    }))
+}
 
 async function getData(slug) {
     const res = await fetch(process.env.NEXT_PUBLIC_BASE_URL + `/rcms-api/1/newsdetail/${slug}`, { cache: 'no-store' });
     return res.json()
 }
 
-export default async function Page(props) {
-    const data = await getData(props.params.slug)
+export default async function Page(params) {
+    const data = await getData(params.slug)
 
     return (
         <div>
-            <h1>{data.details.subject}</h1>
-            <div dangerouslySetInnerHTML={{ __html: data.details.contents }} />
+            <h1>{params.slug}</h1>
         </div>
     );
 }
