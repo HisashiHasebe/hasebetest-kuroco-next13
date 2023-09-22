4a5a0494886dc34694dfb6551d0388720d77a7e8
YuseiAbe <yabe@diverta.co.jp>
2023-09-22 16:33:48 +0900

    Revert "test"
    This reverts commit 92be219ca2b143f28ab596823e6416ba3c3eab77.

--- app/news/[slug]/page.jsx
+++ app/news/[slug]/page.jsx
@@ -1,22 +1,18 @@
 // Dynamic routes; create a page for each ticket ID
-export async function generateStaticParams() {
-    const contents = await fetch(process.env.NEXT_PUBLIC_BASE_URL + '/rcms-api/1/news').then((res) => res.json())
-    return contents.list.map((content) => ({
-        slug: `${content.topics_id}`,
-    }))
-}
+'use client';
 
 async function getData(slug) {
     const res = await fetch(process.env.NEXT_PUBLIC_BASE_URL + `/rcms-api/1/newsdetail/${slug}`, { cache: 'no-store' });
     return res.json()
 }
 
-export default async function Page(params) {
-    const data = await getData(params.slug)
+export default async function Page(props) {
+    const data = await getData(props.params.slug)
 
     return (
         <div>
-            <h1>{params.slug}</h1>
+            <h1>{data.details.subject}</h1>
+            <div dangerouslySetInnerHTML={{ __html: data.details.contents }} />
         </div>
     );
 }
