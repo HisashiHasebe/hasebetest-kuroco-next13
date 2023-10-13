b31d48a4e41696ed695a7f80c7933e1d4f6b3aa3
HisashiHasebe <hhasebe@diverta.co.jp>
2023-10-13 16:29:56 +0800

    update:プレビューページ実装

--- app/api/preview/route.tsx
+++ app/api/preview/route.tsx
@@ -1,14 +1,7 @@
-export const dynamic = 'force-static'
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-    //console.log(req)
-    console.log(req.nextUrl)
-    //console.log(req.url)
-    console.log(req.nextUrl.searchParams)
-    console.log(req.nextUrl.href)
-    //const searchParams = req.nextUrl.searchParams
-    //const query = searchParams.get('preview_token')
-    const res = await fetch(process.env.NEXT_PUBLIC_BASE_URL + '/rcms-api/1/news/preview?preview_token=').then((res) => res.json());
+    const token = req.nextUrl.searchParams.get('preview_token')
+    const res = await fetch(process.env.NEXT_PUBLIC_BASE_URL + '/rcms-api/1/news/preview?preview_token=' + token).then((res) => res.json());
     return NextResponse.json(res)
 }
--- app/news_preview/page.jsx
+++ app/news_preview/page.jsx
@@ -9,7 +9,7 @@ export default function Page(props) {
     // このコードブロックは初回レンダリング時にのみ実行されます
     const fetchData = async () => {
       try {
-        const res = await fetch('/api/preview/?preview_token='+props.searchParams.preview_token).then((res) => res.json());
+        const res = await fetch('/api/preview/?preview_token=' + props.searchParams.preview_token).then((res) => res.json());
         setData(res);
       } catch (error) {
         console.error(error);
@@ -19,9 +19,15 @@ export default function Page(props) {
     fetchData();
   }, []); // 空の依存配列を渡すと、初回レンダリング時のみ実行されます
 
+  //dataの取得完了までLoadingを表示する
+  if (data === null) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-      <h1>test</h1>
+      <h1>{data.details.subject}</h1>
+      <div dangerouslySetInnerHTML={{ __html: data.details.contents }} />
     </div>
   );
 }
